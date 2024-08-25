a876a3dbcc83498c964d1ffbe129e5b1057323d7
Fabien Dehopré <fabien@dehopre.com>
2024-08-25 21:57:35 +0200

    create frontend project

--- MyNetatmo24.AppHost/Program.cs
+++ MyNetatmo24.AppHost/Program.cs
@@ -1,5 +1,24 @@
+using Microsoft.Extensions.Hosting;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.MyNetatmo24_Backend>("Backend");
+var cache = builder.AddRedis("cache");
+
+var backend = builder.AddProject<Projects.MyNetatmo24_Backend>("Backend");
+
+var frontend = builder.AddNpmApp("Frontend", "../MyNetatmo24.Frontend")
+    .WithReference(backend)
+    .WithReference(cache)
+    .WithHttpEndpoint(4200)
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
+var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"] ??
+                    builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
+if (builder.Environment.IsDevelopment() && launchProfile == "https")
+{
+    // Disable TLS certificate validation in development, see https://github.com/dotnet/aspire/issues/3324 for more details.
+    frontend.WithEnvironment("NODE_TLS_REJECT_UNAUTHORIZED", "0");
+}
 
 builder.Build().Run();
