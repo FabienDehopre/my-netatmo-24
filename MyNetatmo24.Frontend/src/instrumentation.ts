ade9b739448402d2dc3fabf2202d32fb58cb3abb
Fabien Dehopré <fabien@dehopre.com>
2024-10-05 14:28:58 +0200

    configure esbuild to send aspire otel config to frontend

---
+++ MyNetatmo24.Frontend/src/instrumentation.ts
@@ -0,0 +1,78 @@
+import {
+  BatchSpanProcessor,
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+  WebTracerProvider
+} from "@opentelemetry/sdk-trace-web";
+import {OTLPTraceExporter} from "@opentelemetry/exporter-trace-otlp-http";
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import {registerInstrumentations} from "@opentelemetry/instrumentation";
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import {isDevMode} from "@angular/core";
+import {Resource} from "@opentelemetry/resources";
+import {ATTR_SERVICE_NAME} from "@opentelemetry/semantic-conventions";
+
+function parseDelimitedValues(s: string): Record<string, string> {
+  const headers = s.split(","); // Split by comma
+  const o: Record<string, string> = {};
+
+  headers.forEach((header) => {
+    const [key, value] = header.split("="); // Split by equal sign
+    o[key.trim()] = value.trim(); // Add to the object, trimming spaces
+  });
+
+  return o;
+}
+
+export function initializeTelemetry(otlpUrl: string,
+                                    headers: string,
+                                    resourceAttributes: string,
+                                    serviceName: string): void {
+  const provider = new WebTracerProvider({
+    resource: new Resource({
+      ...parseDelimitedValues(resourceAttributes),
+      [ATTR_SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  if (isDevMode()) {
+    provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+  }
+
+  provider.addSpanProcessor(
+    new BatchSpanProcessor(
+      new OTLPTraceExporter({
+        url: `${otlpUrl}/v1/traces`,
+        headers: parseDelimitedValues(headers),
+      })
+    )
+  );
+
+  provider.register({
+    contextManager: new ZoneContextManager(),
+  });
+
+  registerInstrumentations({
+    instrumentations: [
+      getWebAutoInstrumentations({
+        '@opentelemetry/instrumentation-document-load': {},
+        '@opentelemetry/instrumentation-user-interaction': {
+          eventNames: ["click", "submit"],
+          shouldPreventSpanCreation: (_, element) => {
+            return (
+              element.tagName === "A" ||
+              (element.tagName === "BUTTON" &&
+                element.getAttribute("type") === "submit")
+            );
+          },
+        },
+        '@opentelemetry/instrumentation-fetch': {
+          propagateTraceHeaderCorsUrls: [new RegExp(`\\/api\\/*`)]
+        },
+        '@opentelemetry/instrumentation-xml-http-request': {
+          propagateTraceHeaderCorsUrls: [new RegExp(`\\/api\\/*`)]
+        },
+      })
+    ]
+  });
+}
--- MyNetatmo24.Frontend/src/main.ts
+++ MyNetatmo24.Frontend/src/main.ts
@@ -1,8 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeTelemetry } from './instrumentation';
 
-import './instrument';
+declare const OTEL_EXPORTER_OTLP_ENDPOINT: string;
+declare const OTEL_EXPORTER_OTLP_HEADERS: string;
+declare const OTEL_RESOURCE_ATTRIBUTES: string;
+declare const OTEL_SERVICE_NAME: string;
+
+initializeTelemetry(
+  OTEL_EXPORTER_OTLP_ENDPOINT,
+  OTEL_EXPORTER_OTLP_HEADERS,
+  OTEL_RESOURCE_ATTRIBUTES,
+  OTEL_SERVICE_NAME);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
