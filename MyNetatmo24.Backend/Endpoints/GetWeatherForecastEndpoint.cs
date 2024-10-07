6456fbdfc2c5b2d1fc1ee54835c0de9ff66af8e4
Fabien Dehopré <fabien@dehopre.com>
2024-10-07 10:57:21 +0200

    move demo endpoint to fast-endpoint

---
+++ MyNetatmo24.Backend/Endpoints/GetWeatherForecastEndpoint.cs
@@ -0,0 +1,54 @@
+using FastEndpoints;
+
+namespace MyNetatmo24.Backend.Endpoints;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public class GetWeatherForecastEndpoint : Ep.NoReq.Res<IEnumerable<WeatherForecast>>
+{
+    public override void Configure()
+    {
+        Get("/weatherforecast");
+        Policies("ReadWeather");
+        Description(d => 
+            d.Produces<IEnumerable<WeatherForecast>>());
+        Summary(s =>
+        {
+            s.Summary = "Returns the weather forecast";
+            s.Description = "Returns the weather forecast for the next 5 days";
+            s.Responses[StatusCodes.Status200OK] = "The weather forecast is returned";
+        });
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        await SendOkAsync(forecast, ct);
+    }
+}
+
+/*
+
+   
+   app.MapGet("/weatherforecast", () =>
+   {
+       
+   })
+   .WithName("GetWeatherForecast")
+   .WithOpenApi()
+   .RequireAuthorization("ReadWeather");
+*/
--- MyNetatmo24.Backend/Program.cs
+++ MyNetatmo24.Backend/Program.cs
@@ -1,4 +1,6 @@
 using System.Security.Claims;
+using FastEndpoints;
+using FastEndpoints.Swagger;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
@@ -7,9 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Configure Aspire services
 builder.AddServiceDefaults();
 
+builder.Services
+    .AddFastEndpoints()
+    .SwaggerDocument();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 // Add authentication and authorization using Auth0
 builder.Services.AddAuthentication(options =>
@@ -39,41 +45,12 @@ var app = builder.Build();
 // Configure Aspire default endpoints
 app.MapDefaultEndpoints();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi()
-.RequireAuthorization("ReadWeather");
+app.UseFastEndpoints()
+    .UseSwaggerGen();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
