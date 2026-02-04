c53b8167ef171268915e253e563dbef4d8dab0ea
Fabien Dehopré <fabien@dehopre.com>
2026-02-04 15:26:20 +0100

    set frontend port to 4200 so authentication works
    also remove shenanigans about the node TLS stuff since aspire now provide the additional CA certificates path to node

--- MyNetatmo24.AppHost/Program.cs
+++ MyNetatmo24.AppHost/Program.cs
@@ -8,19 +8,11 @@ var frontend = builder.AddJavaScriptApp("frontend", "../MyNetatmo24.Frontend")
     .WithReference(backend)
     .WithPnpm(install: true, installArgs: ["--frozen-lockfile"])
     .WithRunScript("start")
-    .WithHttpEndpoint(env: "PORT")
+    .WithHttpEndpoint(port: 4200, env: "PORT")
     .WithEnvironment("APPLICATION", "MyNetatmo24.Frontend")
     .PublishAsDockerFile(configure: resource =>
     {
         resource.WithDockerfile("../", stage: "frontend-app");
     });
-
-var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"]; // ??
-                    // builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
-if (builder.Environment.IsDevelopment() && launchProfile == "https")
-{
-    // Disable TLS certificate validation in development, see https://github.com/dotnet/aspire/issues/3324 for more details.
-    frontend.WithEnvironment("NODE_TLS_REJECT_UNAUTHORIZED", "0");
-}
 
 builder.Build().Run();
