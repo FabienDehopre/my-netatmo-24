8a7c9227089b7198d1090fc562c6f463bad1e91c
Fabien Dehopré <fabien@dehopre.com>
2026-02-08 19:59:17 +0100

    fix all build warnings and errors

--- MyNetatmo24.Frontend/vite.config.mjs
+++ MyNetatmo24.Frontend/vite.config.mjs
@@ -1,23 +1,56 @@
 import angular from '@analogjs/vite-plugin-angular';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+// Patch @protobufjs/inquire library to work in Vite.
+function protobufPatch() {
+  return {
+    name: 'protobuf-patch',
+    transform(code, id) {
+      // https://github.com/protobufjs/protobuf.js/issues/1754
+      if (id.endsWith('@protobufjs/inquire/index.js')) {
+        return {
+          code: code.replace(`eval("quire".replace(/^/,"re"))`, 'require'),
+          // eslint-disable-next-line unicorn/no-null
+          map: null,
+        };
+      }
+    },
+  };
+}
 
 export default defineConfig({
   resolve: {
-    mainFields: ['module'],
+    mainFields: ['module', 'browser'],
   },
   plugins: [
     angular(),
-    tailwindcss(),
     viteTsConfigPaths(),
+    tailwindcss(),
+    // Polyfill Node.js built-in modules for browser compatibility.
+    nodePolyfills(),
+    protobufPatch(),
   ],
   server: {
     port: 4200,
     open: false,
   },
   build: {
+    sourceMap: true,
     target: ['es2022'],
     outDir: 'dist',
+    chunkSizeWarningLimit: 750,
+    rollupOptions: {
+      onLog(level, log, handler) {
+        // Remove warning about source map errors, which we don't care about.
+        if (log.cause?.message === `Can't resolve original location of error.`) {
+          return;
+        }
+
+        handler(level, log);
+      },
+    },
   },
 });
