6d927cf5fac1008af196b15cb08801a5b132d424
Fabien Dehopré <fabien@dehopre.com>
2026-02-16 11:01:08 +0100

    add db migrations for account management module

---
+++ MyNetatmo24.Modules.AccountManagement/Migrations/20260216095849_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyNetatmo24.Modules.AccountManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "accountmamangement");
+
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                schema: "accountmamangement",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Auth0Id = table.Column<string>(type: "text", nullable: true),
+                    NickName = table.Column<string>(type: "text", nullable: true),
+                    AvatarUrl = table.Column<string>(type: "text", nullable: true),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Name_FirstName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    Name_LastName = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    NetatmoAuthInfo_AccessToken = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    NetatmoAuthInfo_ExpiresAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    NetatmoAuthInfo_RefreshToken = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Auth0Id",
+                schema: "accountmamangement",
+                table: "Accounts",
+                column: "Auth0Id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts",
+                schema: "accountmamangement");
+        }
+    }
+}
