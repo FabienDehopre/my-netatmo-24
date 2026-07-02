7731b4766dabf530e10144aab4658e54693ea0e8
Fabien Dehopré <fabien@dehopre.com>
2026-07-02 15:11:27 +0200

    refactor: use tanstack angular table to display the weather forecast

--- MyNetatmo24.Frontend/src/app/app.ts
+++ MyNetatmo24.Frontend/src/app/app.ts
@@ -3,6 +3,7 @@ import { httpResource } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
+import { createAngularTable, createColumnHelper, FlexRenderDirective, getCoreRowModel } from '@tanstack/angular-table';
 import * as z from 'zod/mini';
 
 import { HlmButtonImports } from '@spartan-ng/helm/button';
@@ -20,24 +21,47 @@ const WEATHER_FORECAST_SCHEMA = z.strictObject({
 });
 type WeatherForecast = z.infer<typeof WEATHER_FORECAST_SCHEMA>;
 
+// eslint-disable-next-line @typescript-eslint/naming-convention -- function
+const columnHelper = createColumnHelper<WeatherForecast>();
+const WEATHER_FORECAST_COLUMNS = [
+  columnHelper.accessor('date', {
+    header: 'Date',
+  }),
+  columnHelper.accessor('temperatureC', {
+    header: 'Temp. (C)',
+  }),
+  columnHelper.accessor('temperatureF', {
+    header: 'Temp. (F)',
+  }),
+  columnHelper.accessor('summary', {
+    header: 'Summary',
+  }),
+];
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Anonymous, Authenticated, DatePipe, HlmButtonImports, HlmDropdownMenuImports, NgIcon],
+  imports: [RouterOutlet, Anonymous, Authenticated, DatePipe, HlmButtonImports, HlmDropdownMenuImports, NgIcon, FlexRenderDirective],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class App {
   readonly #authentication = inject(Authentication);
-  protected readonly title = signal('My Netatmo 24').asReadonly();
-  protected readonly user = this.#authentication.user;
-  protected readonly weatherForecasts = httpResource<WeatherForecast[]>(
+  readonly #weatherForecasts = httpResource<WeatherForecast[]>(
     () => '/api/weatherforecast',
     {
       parse: parse(z.array(WEATHER_FORECAST_SCHEMA)),
       defaultValue: [],
     }
   );
+
+  protected readonly title = signal('My Netatmo 24').asReadonly();
+  protected readonly user = this.#authentication.user;
+  protected readonly table = createAngularTable(() => ({
+    data: this.#weatherForecasts.value(),
+    columns: WEATHER_FORECAST_COLUMNS,
+    getCoreRowModel: getCoreRowModel(),
+  }));
 
   protected login(): void {
     this.#authentication.login('/');
@@ -48,6 +72,6 @@ export class App {
   }
 
   protected loadWeatherForecast(): void {
-    this.weatherForecasts.reload();
+    this.#weatherForecasts.reload();
   }
 }
