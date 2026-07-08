0b7bdd76661a22935a33c21d2e2eb4a0e79765d2
Fabien Dehopré <fabien@dehopre.com>
2026-07-08 21:48:13 +0200

    test: create integration tests project with client created using kiota and openapi spec

---
+++ MyNetatmo24.Modules.AccountManagement.IntegrationTests/ModuleInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MyNetatmo24.Modules.AccountManagement.IntegrationTests;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        VerifierSettings.ScrubMember("Id");
+    }
+}
